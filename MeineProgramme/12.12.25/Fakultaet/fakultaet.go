508adc893c4b485f39f06d4f0cb1c1fa6445cd99
SebastianLorenz <sebastian.lorenz.1@gmx.de>
2025-12-12 11:24:16 +0100

    FakultaetV2

---
+++ MeineProgramme/12.12.25/Fakultaet/fakultaet.go
@@ -0,0 +1,19 @@
+package main
+
+func Factorial(n int) int {
+	result := 1
+
+	for i := n; i > 1; i-- {
+
+		result = result * i
+
+	}
+	return result
+}
+
+func FactorialV2(n int) int {
+	if n == 1 {
+		return 1
+	}
+	return n * FactorialV2(n-1)
+}
