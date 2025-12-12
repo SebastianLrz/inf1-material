eafe22084a026df911984e7dd8663bafc86a3185
SebastianLorenz <sebastian.lorenz.1@gmx.de>
2025-12-12 13:02:07 +0100

    Primes3

--- MeineProgramme/12.12.25/primes/is_prime.go
+++ MeineProgramme/12.12.25/primes/is_prime.go
@@ -2,13 +2,17 @@ package primes
 
 func IsPrime(n int) bool {
 
+	if n < 2 {
+		return false
+	}
+
 	for i := 2; i < n; i++ {
 
 		if n%i == 0 {
 			return false
 		}
 	}
-	return n < 2
+	return true
 }
 
 // liefert eine Liste mmit allen Primzahlen bis n
--- MeineProgramme/12.12.25/primes/is_prime_test.go
+++ MeineProgramme/12.12.25/primes/is_prime_test.go
@@ -6,4 +6,9 @@ func ExamplePrimes() {
 	fmt.Println(Primes(10))
 
 	//Output:
+	// 2
+	// 3
+	// 5
+	// 7
+
 }
