71fb2e1438d1c9dbce9e0a31cd1dba8504b83d4b
SebastianLorenz <sebastian.lorenz.1@gmx.de>
2025-12-12 10:24:07 +0100

    fizzbuzz allgemein

--- Meine Programme/12.12.25/Fizzbuzz/fizzbuzz.go
+++ Meine Programme/12.12.25/Fizzbuzz/fizzbuzz.go
@@ -5,6 +5,20 @@ import "fmt"
 func main() {
 	//fizzbuzz_v1()
 	fizzbuzz_v3()
+
+	var a int
+	fmt.Print("Wähle x!")
+	fmt.Scan(&a)
+
+	var b int
+	fmt.Print("Wähle y!")
+	fmt.Scan(&b)
+
+	var m int
+	fmt.Print("Bis wohin soll gezählt werden?")
+	fmt.Scan(&m)
+
+	fizzbuzz_allgemein(a, b, m)
 }
 
 func fizzbuzz_v1() {
@@ -66,6 +80,28 @@ func fizzbuzz_v4() {
 			fmt.Print(i)
 		}
 		fmt.Println()
+
+	}
+}
+
+func fizzbuzz_allgemein(a, b, m int) {
+
+	for i := 0; i < m; i++ {
+
+		if i%a == 0 && i%b == 0 {
+			fmt.Println("fizzbuzz")
+			continue
+		}
+
+		if i%a == 0 {
+			fmt.Println("fizz")
+			continue
+		}
+
+		if i%b == 0 {
+			fmt.Println("buzz")
+			continue
+		}
 
 	}
 }
